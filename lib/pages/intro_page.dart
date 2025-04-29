2c1552eff5501c3895a65d66ae9493d3677d5624
ZunairaMughal24 <168114331+ZunairaMughal24@users.noreply.github.com>
2025-04-29 08:54:36 -0700

    Update intro_page.dart

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,61 +8,64 @@ class MyIntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Image.asset("assets/images/nike4.jpg", height: 200),
-              ),
-              const SizedBox(
-                height: 48,
-              ),
-              //title
-              const Text("Just Do It",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  )),
-              const SizedBox(
-                height: 24,
-              ),
-              //subtitle
-              const Text(
-                "Brand new sneakers and custom kicks made witth premium quality",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Image.asset("assets/images/nike4.jpg", height: 200),
                 ),
-                textAlign: TextAlign.center,
-              ),
-              //start now button
-              const SizedBox(
-                height: 48,
-              ),
-              GestureDetector(
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHome())),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    color: Colors.black,
+                const SizedBox(
+                  height: 48,
+                ),
+                //title
+                const Text("Just Do It",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                const SizedBox(
+                  height: 24,
+                ),
+                //subtitle
+                const Text(
+                  "Brand new sneakers and custom kicks made witth premium quality",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(23),
-                    child: Center(
-                      child: Text(
-                        "Shop now",
-                        style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                //start now button
+                const SizedBox(
+                  height: 48,
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const MyHome())),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: Colors.black,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(23),
+                      child: Center(
+                        child: Text(
+                          "Shop now",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
